7e3461a73371284701f226bf6e8ed6d06eb68f91
Andrei Miculita <andreimiculita@gmail.com>
2021-10-02 15:12:59 +0200

    Visualizing mesh

--- load_example_mesh.py
+++ load_example_mesh.py
@@ -1,9 +1,11 @@
 import open3d as o3d
 
-mesh_path = "data/ModelNet10/bed/train/bed_0001.off"
+mesh_path = "data/ModelNet10/bed/train/bed_0002.off"
 
 mesh = o3d.io.read_triangle_mesh(mesh_path)
 
 vertices = mesh.vertices
 faces = mesh.triangles
+mesh.compute_vertex_normals()
+o3d.visualization.draw_geometries([mesh])
 
