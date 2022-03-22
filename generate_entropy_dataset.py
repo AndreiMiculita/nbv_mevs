a80fca1c1fa199f454db41ec9fb81f72704bfdbe
Andrei Miculita <andreimiculita@gmail.com>
2022-03-22 21:51:44 +0100

    Change script for generating entropy dataset to have more uniformly spaced views

--- generate_entropy_dataset.py
+++ generate_entropy_dataset.py
@@ -23,6 +23,8 @@ import numpy as np
 import pandas as pd
 from skimage.measure import shannon_entropy
 import matplotlib.pyplot as plt
+from sympy import pi, sin, cos, sqrt, acos, atan2
+import math
 
 parser = argparse.ArgumentParser(description="Generates a dataset in CSV format of depth-views entropy values.")
 parser.add_argument("--modelnet10", help="Specify root directory to the ModelNet10 dataset.", required=True)
@@ -56,6 +58,51 @@ N_VIEWS_W = args.horizontal_split
 N_VIEWS_H = args.vertical_split
 VIEW_INDEX = 0
 FIRST_OBJECT = 1
+
+
+# credit: https://stackoverflow.com/a/26127012/13200217
+def fibonacci_sphere(samples=1000):
+    points = []
+    phi = math.pi * (3. - math.sqrt(5.))  # golden angle in radians
+
+    for i in range(samples):
+        y = 1 - (i / float(samples - 1)) * 2  # y goes from 1 to -1
+        radius = math.sqrt(1 - y * y)  # radius at y
+
+        theta = phi * i  # golden angle increment
+
+        x = math.cos(theta) * radius
+        z = math.sin(theta) * radius
+
+        # Reorder so first points are on the sides
+        (x, y, z) = (y, x, z)
+
+        points.append((x, y, z))
+
+    return points
+
+
+# credit https://stackoverflow.com/a/43893134/13200217
+def as_cartesian(rthetaphi):
+    #takes list rthetaphi (single coord)
+    r       = rthetaphi[0]
+    theta   = rthetaphi[1]* pi/180 # to radian
+    phi     = rthetaphi[2]* pi/180
+    x = r * sin( theta ) * cos( phi )
+    y = r * sin( theta ) * sin( phi )
+    z = r * cos( theta )
+    return [x,y,z]
+
+def as_spherical(xyz):
+    #takes list xyz (single coord)
+    x       = xyz[0]
+    y       = xyz[1]
+    z       = xyz[2]
+    r       =  np.float(sqrt(x*x + y*y + z*z))
+    theta   =  np.float(acos(z/r)*180/ pi) #to degrees
+    phi     =  np.float(atan2(y,x)*180/ pi)
+    return [r,theta,phi]
+
 
 
 def nonblocking_custom_capture(tr_mesh, rot_xyz, last_rot=(0, 0, 0)):
@@ -126,11 +173,13 @@ for split_set in ['train', 'test']:
             if args.debug:
                 print(f"[DEBUG] Current Object: {file}")
 
+            initial_views = fibonacci_sphere(10)
+
             rotations = []
-            for j in range(0, N_VIEWS_H):
-                for i in range(N_VIEWS_W):
-                    # Excluding 'rings' on 0 and 180 degrees since it would be the same projection but rotated
-                    rotations.append((-(j + 1) * np.pi / (N_VIEWS_H + 1), 0, i * 2 * np.pi / N_VIEWS_W))
+
+            for initial_view in initial_views:
+                rotations.append(as_spherical(initial_view))
+
             last_rotation = (0, 0, 0)
             for rot in rotations:
                 nonblocking_custom_capture(mesh, rot, last_rotation)
