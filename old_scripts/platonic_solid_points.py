1eb0a2483d1748b1b4f62302b14ad11dbdb6df84
Andrei Miculita <andreimiculita@gmail.com>
2022-08-31 20:49:39 +0200

    Run auto-indent on platonic_solid_points.py

--- old_scripts/platonic_solid_points.py
+++ old_scripts/platonic_solid_points.py
@@ -14,19 +14,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from plotting_utils import set_axes_equal, show_3d_axes_rgb
 
+phi = (1 + np.sqrt(5)) / 2  # the golden ratio, appears in some coordinates
 
-phi = (1 + np.sqrt(5)) / 2  # the golden ratio, appears in some of the coordinates
 
-
-def make_tetrahedron_vertices(circumradius=np.sqrt(6)/2) -> np.array:
+def make_tetrahedron_vertices(circumradius=np.sqrt(6) / 2) -> np.array:
     # Based on https://en.wikipedia.org/wiki/Tetrahedron#Coordinates_for_a_regular_tetrahedron
     # Default circumradius leads to edge length 2
-    a = 2/np.sqrt(6)
+    a = 2 / np.sqrt(6)
 
     vertices = []
     for i in [-1, 1]:
-        vertices.append([i*a*circumradius, 0, -1/np.sqrt(2)*a*circumradius])
-        vertices.append([0, i*a*circumradius,  1/np.sqrt(2)*a*circumradius])
+        vertices.append([i * a * circumradius, 0, -1 / np.sqrt(2) * a * circumradius])
+        vertices.append([0, i * a * circumradius, 1 / np.sqrt(2) * a * circumradius])
 
     return np.array(vertices)
 
@@ -40,7 +39,7 @@ def make_cube_vertices(circumradius=np.sqrt(3)) -> np.array:
     for i in [-1, 1]:
         for j in [-1, 1]:
             for k in [-1, 1]:
-                vertices.append([i*a*circumradius, j*a*circumradius, k*a*circumradius])
+                vertices.append([i * a * circumradius, j * a * circumradius, k * a * circumradius])
 
     return np.array(vertices)
 
@@ -50,9 +49,9 @@ def make_octahedron_vertices(circumradius=1) -> np.array:
     # Default circumradius leads to edge length sqrt(2)
     vertices = []
     for i in [-1, 1]:
-        vertices.append([i*circumradius, 0, 0])
-        vertices.append([0, i*circumradius, 0])
-        vertices.append([0, 0, i*circumradius])
+        vertices.append([i * circumradius, 0, 0])
+        vertices.append([0, i * circumradius, 0])
+        vertices.append([0, 0, i * circumradius])
 
     return np.array(vertices)
 
@@ -72,41 +71,39 @@ def make_dodecahedron_vertices(circumradius=np.sqrt(3)) -> np.array:
             vertices.append([i * c * circumradius, j * b * circumradius, 0])
             vertices.append([i * b * circumradius, 0, j * c * circumradius])
             for k in [-1, 1]:
-                vertices.append([i*a*circumradius, j*a*circumradius, k*a*circumradius])
+                vertices.append([i * a * circumradius, j * a * circumradius, k * a * circumradius])
 
     return np.array(vertices)
 
 
-def make_icosahedron_vertices(circumradius=np.sqrt(phi+2)) -> np.array:
+def make_icosahedron_vertices(circumradius=np.sqrt(phi + 2)) -> np.array:
     # Based on https://en.wikipedia.org/wiki/Regular_icosahedron#Cartesian_coordinates
     # Default circumradius leads to edge length 2
-    a = 1 / np.sqrt(phi+2)
+    a = 1 / np.sqrt(phi + 2)
     c = a * phi
 
     vertices = []
     for i in [-1, 1]:
         for j in [-1, 1]:
-            vertices.append([0, i*a*circumradius, j*c*circumradius])
-            vertices.append([i*a*circumradius, j*c*circumradius, 0])
-            vertices.append([j*c*circumradius, 0, i*a*circumradius])
+            vertices.append([0, i * a * circumradius, j * c * circumradius])
+            vertices.append([i * a * circumradius, j * c * circumradius, 0])
+            vertices.append([j * c * circumradius, 0, i * a * circumradius])
     return np.array(vertices)
 
 
 def main():
     # make a 3d plot of each function's output and save them in separate images
     for func in [make_tetrahedron_vertices, make_cube_vertices, make_octahedron_vertices,
-                    make_dodecahedron_vertices, make_icosahedron_vertices]:
-            vertices = func()
-            print(vertices)
-            ax = plt.axes(projection='3d')
-            ax.scatter3D(*vertices.transpose(), s=100, c='b')
-            ax.set_box_aspect([1, 1, 1])
-            set_axes_equal(ax)
-            show_3d_axes_rgb(ax)
-            # plt.show()
-            plt.savefig(func.__name__.replace("make_", "") + '.pdf', bbox_inches='tight')
-
-
+                 make_dodecahedron_vertices, make_icosahedron_vertices]:
+        vertices = func()
+        print(vertices)
+        ax = plt.axes(projection='3d')
+        ax.scatter3D(*vertices.transpose(), s=100, c='b')
+        ax.set_box_aspect([1, 1, 1])
+        set_axes_equal(ax)
+        show_3d_axes_rgb(ax)
+        plt.show()
+        # plt.savefig(func.__name__.replace("make_", "") + '.pdf', bbox_inches='tight')
 
 
 if __name__ == '__main__':
