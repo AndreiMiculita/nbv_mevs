e3c90507962db92fa3d21ccd153365e087a2aa58
Andrei Miculita <andreimiculita@gmail.com>
2022-10-09 00:31:18 +0200

    Add diagram from 10-point sphere, try fitting SOM again (not working)

--- view_entropymap_projected.py
+++ view_entropymap_projected.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from plotting_utils import set_axes_equal, show_3d_axes_rgb
 from sklearn_som.som import SOM
 
+
 # credit: https://stackoverflow.com/a/26127012/13200217
 def fibonacci_sphere(samples=1000):
     points = []
@@ -56,7 +57,23 @@ def as_spherical(xyz):
 
 
 if __name__ == "__main__":
-    # generate a sphere
+    # generate a sphere of 10 points
+    points = fibonacci_sphere(samples=10)
+
+    # convert to numpy array
+    points = np.array(points)
+
+    print(points)
+
+    # view the sphere points as interactive 3d scatter plot
+    ax = plt.axes(projection="3d")
+    ax.scatter3D(points[:, 0], points[:, 1], points[:, 2], s=100, c='b')
+    ax.set_box_aspect([1, 1, 1])
+    set_axes_equal(ax)
+    show_3d_axes_rgb(ax)
+    plt.savefig("fibonacci_sphere_points_10.pdf", bbox_inches='tight')
+
+    # generate a sphere of 40 points
     points = fibonacci_sphere(samples=40)
 
     # convert to numpy array
@@ -66,11 +83,11 @@ if __name__ == "__main__":
 
     # view the sphere points as interactive 3d scatter plot
     ax = plt.axes(projection="3d")
-    ax.scatter3D(points[:, 0], points[:, 1], points[:, 2])
+    ax.scatter3D(points[:, 0], points[:, 1], points[:, 2], s=100, c='b')
     ax.set_box_aspect([1, 1, 1])
     set_axes_equal(ax)
     show_3d_axes_rgb(ax)
-    plt.show()
+    plt.savefig("fibonacci_sphere_points_40.pdf", bbox_inches='tight')
 
     # convert to spherical coordinates as np array
 
@@ -101,18 +118,30 @@ if __name__ == "__main__":
     spherical[:, 0] = -spherical[:, 0] * 180 / pi
     spherical[:, 2] = spherical[:, 2] * 180 / pi
 
+    # clear the figure
+    plt.clf()
+
     # view the spherical coords on a 2d scatter plot
     ax = plt.axes()
     ax.scatter(spherical[:, 2], spherical[:, 0])
     ax.set_ylabel("theta")
     ax.set_xlabel("phi")
+    # equal axes
+    ax.set_aspect('equal', adjustable='box')
+
+    plt.savefig("fibonacci_sphere_spherical_coords_2d.pdf", bbox_inches='tight')
+
+    # Fit an SOM to map the spherical coordinates to a 2d grid
+    # Unfortunately not working
+    som = SOM(m=7, n=7, dim=2)
+    # Fit just columns 2 and 0
+    som.fit(spherical[:, [2, 0]])
+
+    # view the SOM
+    ax = plt.axes()
+    ax.scatter(som.weights[:, 0], som.weights[:, 1])
+
+    print(som.weights)
+    ax.set_ylabel("theta")
+    ax.set_xlabel("phi")
     plt.show()
-
-    # # Fit SOM
-    # som = SOM(m=7, n=7, dim=2)
-    # # Fit just columns 2 and 1
-    # som.fit(spherical[:, [2, 1]])
-    #
-    # # view the SOM
-    # ax = plt.axes()
-
