a1ee9483939b9f5823f68f78024b1a1f0ae0c841
Andrei Miculita <andreimiculita@gmail.com>
2022-12-23 21:22:02 +0100

    Script for finding local maximum nodes in node weighted graph

---
+++ node_weighted_graph/find_local_maximum_nodes.py
@@ -0,0 +1,31 @@
+import heapq
+
+
+def find_local_maximum_nodes(graph):
+    result = set()
+    priority_queue = []
+
+    # Initialize priority queue with all nodes
+    for node in graph:
+        heapq.heappush(priority_queue, (node.weight, node))
+
+    while priority_queue:
+        current_weight, current_node = heapq.heappop(priority_queue)
+
+        # Check if current node has higher weight than neighbors
+        is_higher_weight = all(current_node.weight > neighbor.weight for neighbor in current_node.neighbors)
+        if is_higher_weight:
+            result.add(current_node)
+
+        # Remove neighbors with lower weights from priority queue
+        updated_neighbors = []
+        for neighbor in current_node.neighbors:
+            if neighbor.weight > current_node.weight:
+                updated_neighbors.append(neighbor)
+        current_node.neighbors = updated_neighbors
+
+        # Update priority queue with the remaining neighbors
+        for neighbor in current_node.neighbors:
+            heapq.heappush(priority_queue, (neighbor.weight, neighbor))
+
+    return result
