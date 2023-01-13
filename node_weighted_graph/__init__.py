fefc44bb78a20abfafdd129b2550226df2f32fa9
Andrei Miculita <andreimiculita@gmail.com>
2023-01-13 22:02:59 +0100

    Add ability to plot graph with explicitly set node positions

--- node_weighted_graph/__init__.py
+++ node_weighted_graph/__init__.py
@@ -1,25 +1,29 @@
+"""
+Contains functions for handling graphs of nodes with weights.
+"""
+
 from typing import List
 
 import numpy as np
 
 from node_weighted_graph.Node import Node
+from node_weighted_graph.build_graph_from_spherical_coords import build_graph_from_spherical_coords
 from node_weighted_graph.find_local_maximum_nodes import find_local_maximum_nodes
 from node_weighted_graph.generate_graph_image import generate_graph_image
-from node_weighted_graph.build_graph_from_spherical_coords import build_graph_from_spherical_coords
 
 
 def get_example_graph() -> List[Node]:
     # Example graph
-    A = Node('A', 5)
-    B = Node('B', 8)
-    C = Node('C', 31)
-    D = Node('D', 7)
-    E = Node('E', 44)
-    F = Node('F', 6)
-    G = Node('G', 4)
-    H = Node('H', 24)
-    I = Node('I', 11)
-    J = Node('J', 10)
+    A = Node('A', 1, 1, 5)
+    B = Node('B', 2, 0, 8)
+    C = Node('C', 3, 0, 31)
+    D = Node('D', 4, 1, 7)
+    E = Node('E', 5, 1, 44)
+    F = Node('F', 6, 1, 6)
+    G = Node('G', 1, 2, 4)
+    H = Node('H', 2, 2, 24)
+    I = Node('I', 3, 2, 11)
+    J = Node('J', 4, 2, 10)
 
     A.add_neighbor(B)
     A.add_neighbor(C)
@@ -39,50 +43,53 @@ def get_example_graph() -> List[Node]:
 
 
 example_spherical_coords = np.array(
-    [[102.0, 166.0],
-     [102.0, 57.0],
-     [106.0, 232.0],
-     [109.0, 125.0],
-     [109.0, 342.0],
-     [114.0, 302.0],
-     [115.0, 201.0],
-     [116.0, 27.0],
-     [125.0, 81.0],
-     [129.0, 257.0],
-     [134.0, 165.0],
-     [139.0, 349.0],
-     [142.0, 117.0],
-     [148.0, 296.0],
-     [148.0, 43.0],
-     [149.0, 212.0],
-     [15.0, 152.0],
-     [16.0, 310.0],
-     [177.0, 119.0],
-     [32.0, 35.0],
-     [38.0, 217.0],
-     [40.0, 88.0],
-     [45.0, 264.0],
-     [47.0, 151.0],
-     [47.0, 323.0],
-     [58.0, 360.0],
-     [62.0, 116.0],
-     [64.0, 182.0],
-     [64.0, 39.0],
-     [67.0, 291.0],
-     [69.0, 217.0],
-     [73.0, 73.0],
-     [78.0, 250.0],
-     [84.0, 143.0],
-     [87.0, 318.0],
-     [88.0, 18.0],
-     [90.0, 180.0],
-     [90.0, 360.0],
-     [92.0, 97.0],
-     [98.0, 274.0]])
+    [
+        [15.0, 152.0],
+        [16.0, 310.0],
+        [32.0, 35.0],
+        [38.0, 217.0],
+        [40.0, 88.0],
+        [45.0, 264.0],
+        [47.0, 151.0],
+        [47.0, 323.0],
+        [58.0, 360.0],
+        [62.0, 116.0],
+        [64.0, 182.0],
+        [64.0, 39.0],
+        [67.0, 291.0],
+        [69.0, 217.0],
+        [73.0, 73.0],
+        [78.0, 250.0],
+        [84.0, 143.0],
+        [87.0, 318.0],
+        [88.0, 18.0],
+        [90.0, 180.0],
+        [90.0, 360.0],
+        [92.0, 97.0],
+        [98.0, 274.0],
+        [102.0, 166.0],
+        [102.0, 57.0],
+        [106.0, 232.0],
+        [109.0, 125.0],
+        [109.0, 342.0],
+        [114.0, 302.0],
+        [115.0, 201.0],
+        [116.0, 27.0],
+        [125.0, 81.0],
+        [129.0, 257.0],
+        [134.0, 165.0],
+        [139.0, 349.0],
+        [142.0, 117.0],
+        [148.0, 296.0],
+        [148.0, 43.0],
+        [149.0, 212.0],
+        [177.0, 119.0],
+    ])
 
 
 def main():
     graph = build_graph_from_spherical_coords(example_spherical_coords)
+    # graph = get_example_graph()
 
     # Find nodes with higher weights than neighbors
     result = find_local_maximum_nodes(graph)
--- node_weighted_graph/generate_graph_image.py
+++ node_weighted_graph/generate_graph_image.py
@@ -8,38 +8,35 @@ from node_weighted_graph import Node
 
 def generate_graph_image(graph: List[Node]):
     G = nx.Graph()
+    node_pos = {}
 
     # Add nodes with their attributes
     for node in graph:
         G.add_node(node.name, weight=node.weight)
-
-    # Add edges between neighbors
-    for node in graph:
+        node_pos[node.name] = (node.x, node.y)
         for neighbor in node.neighbors:
             G.add_edge(node.name, neighbor.name)
-
-    # Generate the graph layout
-    pos = nx.spring_layout(G)
 
     # Set node colors based on their weights
     node_colors = [node.weight for node in graph]
 
-    # Draw the graph
-    nx.draw(G, pos, with_labels=False, node_color=node_colors, edgecolors='black', cmap='plasma_r', node_size=500, font_size=10)
+    # Draw the graph with specified node positions
+    nx.draw_networkx(G, pos=node_pos, with_labels=False, node_color=node_colors, edgecolors='black', cmap='plasma',
+                     node_size=500)
 
     # Add node weight labels above the nodes
-    label_pos = {k: (v[0], v[1] + 0.1) for k, v in pos.items()}  # Adjust the y-offset as needed
+    label_pos = {k: (v[0], v[1] + 0.1) for k, v in node_pos.items()}  # Adjust the y-offset as needed
     labels = nx.get_node_attributes(G, 'weight')
     nx.draw_networkx_labels(G, label_pos, labels=labels)
 
     # Add node names below the nodes
-    name_pos = {k: (v[0], v[1] - 0.1) for k, v in pos.items()}  # Adjust the y-offset as needed
+    name_pos = {k: (v[0], v[1] - 0.1) for k, v in node_pos.items()}  # Adjust the y-offset as needed
     node_names = {node.name: node.name for node in graph}
-    nx.draw_networkx_labels(G, name_pos, labels=node_names)
+    nx.draw_networkx_labels(G, pos=name_pos, labels=node_names)
 
     # Add legend
-    plt.colorbar(nx.draw_networkx_nodes(G, pos, node_color=node_colors, edgecolors='black', cmap='plasma',
-                               node_size=500))
+    plt.colorbar(nx.draw_networkx_nodes(G, pos=node_pos, node_color=node_colors, edgecolors='black', cmap='plasma',
+                                        node_size=500))
 
     # Display the image
     plt.show()
