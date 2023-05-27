b2c8420120c789564011854a3dc4a1e0361f7bb0
Andrei Miculita <andreimiculita@gmail.com>
2023-05-27 16:18:37 +0200

    Remove duplication of custom_draw_geometry_with_key_callback

--- visualization/plotting_utils.py
+++ visualization/plotting_utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import open3d as o3d
 
 
 # Source: https://stackoverflow.com/a/31364297/13200217
@@ -40,3 +41,25 @@ def show_3d_axes_rgb(ax):
     ax.quiver(x, y, z, u, v, w, arrow_length_ratio=0.1, color='green')
     u, v, w = np.array([[0], [0], [1]])
     ax.quiver(x, y, z, u, v, w, arrow_length_ratio=0.1, color='blue')
+
+
+def custom_draw_geometry_with_key_callback(pcd):
+    """
+    This function is a modified version of the function custom_draw_geometry from the Open3D examples.
+    It allows us to change the background color of the point cloud by pressing the key 'K'
+    :param pcd:
+    :return:
+    """
+    def change_background_to_black(vis):
+        opt = vis.get_render_option()
+        print(opt.background_color)
+        print(type(opt.background_color))
+        if (opt.background_color == np.asarray([1., 1., 1.])).all():
+            opt.background_color = np.asarray([0., 0., 0.])
+        elif (opt.background_color == np.asarray([0., 0., 0.])).all():
+            opt.background_color = np.asarray([1., 1., 1.])
+        return False
+
+    key_to_callback = {ord("K"): change_background_to_black}
+
+    o3d.visualization.draw_geometries_with_key_callbacks([pcd], key_to_callback)
--- visualization/view_rgb_d_as_pointcloud.py
+++ visualization/view_rgb_d_as_pointcloud.py
@@ -1,24 +1,10 @@
 # Visualize RGB-D data as pointcloud
 
 import open3d as o3d
-import numpy as np
 import sys
 
-def custom_draw_geometry_with_key_callback(pcd):
+from visualization.plotting_utils import custom_draw_geometry_with_key_callback
 
-    def change_background_to_black(vis):
-        opt = vis.get_render_option()
-        print(opt.background_color)
-        print(type(opt.background_color))
-        if (opt.background_color == np.asarray([1., 1., 1.])).all():
-            opt.background_color = np.asarray([0., 0., 0.])
-        elif (opt.background_color == np.asarray([0., 0., 0.])).all():
-            opt.background_color = np.asarray([1., 1., 1.])
-        return False
-
-    key_to_callback = {ord("K"): change_background_to_black}
-
-    o3d.visualization.draw_geometries_with_key_callbacks([pcd], key_to_callback)
 
 def view_pcd(rgb_path, depth_path):
     """
@@ -35,7 +21,9 @@ def view_pcd(rgb_path, depth_path):
     depth = o3d.io.read_image(depth_path)
 
     # Convert the RGB and depth images into pointcloud
-    rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(color=o3d.geometry.Image(rgb), depth=o3d.geometry.Image(depth), convert_rgb_to_intensity=False)
+    rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(color=o3d.geometry.Image(rgb),
+                                                              depth=o3d.geometry.Image(depth),
+                                                              convert_rgb_to_intensity=False)
     pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
         image=rgbd,
         intrinsic=o3d.camera.PinholeCameraIntrinsic(o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault))
@@ -44,8 +32,8 @@ def view_pcd(rgb_path, depth_path):
 
     custom_draw_geometry_with_key_callback(pcd)
 
-
     print(pcd)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
