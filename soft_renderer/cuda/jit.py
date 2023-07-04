354ea4390cf759d229bf9d87395c75e685ebd6ad
Andrei Miculita <andreimiculita@gmail.com>
2023-07-04 07:29:50 +0200

    Remove soft renderer, was never used

--- soft_renderer/cuda/jit.py
+++
@@ -1,8 +0,0 @@
-import os.path as osp
-from torch.utils.cpp_extension import load
-
-current_folder = osp.realpath(osp.abspath(osp.dirname(__file__)))
-
-soft_rasterize_cuda = load('soft_rasterize_cuda',
-                           [current_folder + '/soft_rasterize_cuda.cpp', current_folder + '/soft_rasterize_cuda_kernel.cu'],
-                           verbose=True)
