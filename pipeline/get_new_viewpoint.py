b6d717b710e24f6e0f6b92d189f148f729dda36f
Andrei Miculita <andreimiculita@gmail.com>
2023-07-09 06:16:57 +0200

    Convert docstrings to sphinx format in get_new_viewpoint and improve them

--- pipeline/get_new_viewpoint.py
+++ pipeline/get_new_viewpoint.py
@@ -7,24 +7,22 @@ def get_new_viewpoint_coords(mesh_path: Path, attempted_viewpoints: List[Tuple[f
                              possible_viewpoints: List[Tuple[float, float]], method: str = "random") \
         -> Tuple[float, float]:
     """
-    Chooses a new viewpoint for a mesh.
+    Chooses a next best viewpoint for recognizing an object from a mesh.
+     :param mesh_path: The path to the mesh.
+     :param attempted_viewpoints: List of attempted viewpoints, in (theta, phi) format
+     :param possible_viewpoints: List of possible viewpoints
+     :param method: Method for choosing a new viewpoint. Can be "random", "diff", or "pcd"
 
-    Args:
-        mesh : open3d.geometry.TriangleMesh
-        attempted_viewpoints : List[float, float]
-        method (str): Method for choosing a new viewpoint. Can be "random", "diff", or "pcd"
-        possible_viewpoints (int): Only for random method, what number of points to use for fibonacci sphere
-
-    Returns a tuple of floats:
-        theta (float): Azimuthal angle; in radians
-        phi (float): Polar angle; in radians
+    :return: Tuple of floats: (theta, phi)
+     theta: angle in radians [0 - pi]
+     phi: angle in radians [0 - 2pi]
     """
     if method == "random":
         return random.choice(possible_viewpoints)
     elif method == "diff":
-        return with_differentiable_renderer(mesh, attempted_viewpoints)
+        return with_differentiable_renderer(mesh_path, attempted_viewpoints)
     elif method == "pcd":
-        return with_point_cloud_embedding_network(mesh, attempted_viewpoints)
+        return with_point_cloud_embedding_network(mesh_path, attempted_viewpoints)
 
     raise ValueError("Invalid method: {}".format(method))
 
@@ -32,16 +30,14 @@ def get_new_viewpoint_coords(mesh_path: Path, attempted_viewpoints: List[Tuple[f
 def with_differentiable_renderer(mesh_path: Path, attempted_viewpoints: List[Tuple[float, float]]) -> Tuple[float, float]:
     """
     Chooses a new viewpoint for a mesh using differentiable rendering.
+    :param mesh_path: The path to the mesh.
+    :param attempted_viewpoints: List of attempted viewpoints
 
-    Args:
-          mesh : open3d.geometry.TriangleMesh
-          attempted_viewpoints : List[float, float]
-
-    Returns:
-        theta (float): Azimuthal angle
-        phi (float): Polar angle
+    :return: Tuple of floats: (theta, phi)
+     theta: angle in radians [0 - pi]
+     phi: angle in radians [0 - 2pi]
     """
-
+    # Implement the functionality here
     pass
 
 
@@ -50,11 +46,18 @@ def with_point_cloud_embedding_network(mesh_path: Path, attempted_viewpoints: Li
     """
     Choose a new viewpoint for a mesh using point cloud embedding network.
 
-    Args:
+    :param mesh_path: The path to the mesh.
+    :param attempted_viewpoints: List of attempted viewpoints
 
-    Returns:
-        theta (float): Azimuthal angle
-        phi (float): Polar angle
+    :return: Tuple of floats: (theta, phi)
+     theta: angle in radians [0 - pi]
+     phi: angle in radians [0 - 2pi]
     """
-
+    # Get capture of last attempted viewpoint
+    # Run inference with the pointnet on the point cloud retrieved, output is entropy list
+    # Build view graph structure
+    # Find local maxima in view graph, sort them by entropy
+    # Pick the highest entropy view; if already attempted, pick next one and so on; return it
+    # If all the local maxima have been returned, sort all views by entropy,
+    # return the highest one that is not a local maximum
     pass
